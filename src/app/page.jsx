74ac84af1cefbab0a73f886e49a9cff6071b1f03
jsoto007 <jsoto7087@gmail.com>
2025-06-22 14:10:43 -0400

    updates logo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -181,12 +181,10 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-balance text-neutral-950 sm:text-7xl">
-            Award-winning development studio based in Denmark.
+          AI-Driven Software Solutions for Small Businesses.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            We are a development studio working at the intersection of design
-            and technology. It’s a really busy intersection though — a lot of
-            our staff have been involved in hit and runs.
+          We help small businesses and private practices automate repetitive tasks, streamline operations, and unlock new levels of efficiency — so your team can focus on high-value work, not busywork.
           </p>
         </FadeIn>
       </Container>
