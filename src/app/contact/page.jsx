5d3335b088645f6bb968fe543909e22557466d62
jsoto007 <jsoto7087@gmail.com>
2025-07-08 18:34:52 -0400

    updating contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -71,10 +71,10 @@ function ContactForm() {
             <fieldset>
               <legend className="text-base/6 text-neutral-500">Budget</legend>
               <div className="mt-6 grid grid-cols-1 gap-8 sm:grid-cols-2">
-                <RadioInput label="$25K – $50K" name="budget" value="25" />
-                <RadioInput label="$50K – $100K" name="budget" value="50" />
-                <RadioInput label="$100K – $150K" name="budget" value="100" />
-                <RadioInput label="More than $150K" name="budget" value="150" />
+                <RadioInput label="$2.5K – $5K" name="budget" value="2.5" />
+                <RadioInput label="$5K – $10K" name="budget" value="5" />
+                <RadioInput label="$10K – $25K" name="budget" value="10" />
+                <RadioInput label="More than $25K" name="budget" value="25" />
               </div>
             </fieldset>
           </div>
